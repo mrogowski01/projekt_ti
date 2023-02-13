0a4adc4943ce261c2e51231d0bdad1d144128691
mrogowski01 <michal.rogowski2611@gmail.com>
2023-02-13 21:39:27 +0100

    Update registers.php

--- registers.php
+++ registers.php
@@ -10,7 +10,7 @@ $reg = new Register_new;
 $reg->_read();
 // $reg->_write();
 // $reg->_save();
-echo "<p><a href='./'> Powrot do strony glownej</a></p>";
+// echo "<p><a href='./'> Powrot do strony glownej</a></p>";
            
 if($reg->_save() == 'Dane zostaly zapisane'){
   header('Location: logowanie.php');
